089325c52ff82b3cfa064f7e1d34a006271cb756
Phuc Nguyen <66867906+p1401@users.noreply.github.com>
2021-12-10 13:17:05 +0700

    Day 1

---
+++ FapDesktopVersion/FapDesktopVersion/DAO/DAO.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FapDesktopVersion.DAO
+{
+    class DAO
+    {
+        public static SqlConnection GetConnection()
+        {
+            var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            string ConnectionStr = config.GetConnectionString("MyConStr");
+            return new SqlConnection(ConnectionStr);
+        }
+
+        public static DataTable GetDataBySql(string sql, SqlParameter[] parameters = null)
+        {
+            SqlCommand command = new SqlCommand();
+            command.Connection = GetConnection();
+            command.CommandText = sql;
+            if (parameters != null && parameters.Length != 0) 
+                command.Parameters.AddRange(parameters);
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            adapter.SelectCommand = command;
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            return dt;
+        }
+
+        public static int ExecuteSql(string sql, SqlParameter[] parameters = null)
+        {
+            SqlCommand command = new SqlCommand(sql, GetConnection());
+            if (parameters != null && parameters.Length != 0)
+                command.Parameters.AddRange(parameters);
+            command.Connection.Open();
+            int count = command.ExecuteNonQuery();
+            command.Connection.Close();
+            return count;
+        }
+    }
+}
